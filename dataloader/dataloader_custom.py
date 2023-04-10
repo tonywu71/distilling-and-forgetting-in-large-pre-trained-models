d402c18529aee3969a990745d88c3ed02948cb37
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-04-10 18:30:32 +0100

    Add and validate test for `preprocess_dataset`: - Small refacto code into `rename_label_col`

--- dataloader/dataloader_custom.py
+++ dataloader/dataloader_custom.py
@@ -6,16 +6,24 @@ from datasets import DatasetDict, load_dataset
 from utils.constants import DEFAULT_LABEL_COL
 
 
+def rename_label_col(dataset_dict: DatasetDict,
+                     old_label_col: str,
+                     new_label_col: str=DEFAULT_LABEL_COL) -> DatasetDict:
+    """Rename the label column in dataset_dict."""
+    for split in dataset_dict:
+        dataset_dict[split] = dataset_dict[split].rename_column(old_label_col, new_label_col)
+    return dataset_dict
+
+
 def load_librispeech(**kwargs) -> DatasetDict:
     """Load the LibriSpeech dataset."""
-    OLD_LABEL_COL = "text"
+    old_label_col = "text"
     
     dataset_dict = {}
-    
     dataset_dict["train"] = load_dataset("librispeech_asr", name="clean", split="train.100")
     dataset_dict["test"] = load_dataset("librispeech_asr", name="clean", split="test")
+    dataset_dict = DatasetDict(dataset_dict)
     
-    for split in dataset_dict:
-        dataset_dict[split] = dataset_dict[split].rename_column(OLD_LABEL_COL, DEFAULT_LABEL_COL)
+    dataset_dict = rename_label_col(dataset_dict, old_label_col=old_label_col)
     
-    return DatasetDict(dataset_dict)
+    return dataset_dict
