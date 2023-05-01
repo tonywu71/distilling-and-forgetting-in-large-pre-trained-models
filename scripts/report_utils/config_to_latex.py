78aed4f4820cf60a07002120cb6f3a901adaf2e9
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-01 11:47:22 +0100

    Add script for eval comparison with a LaTeX table

--- scripts/report_utils/config_to_latex.py
+++ scripts/report_utils/config_to_latex.py
@@ -1,7 +1,7 @@
 import typer
 
 import os, sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from dataclasses import asdict
 import pandas as pd
@@ -11,7 +11,8 @@ from utils.config import load_yaml_config
 
 def main(config_file: str=typer.Argument(..., help="Path to model config file (.yaml)")):
     """
-    Script to convert a YAML config file to a LaTeX table. To be used for reports.
+    Script to convert a YAML config file to a LaTeX table.
+    To be used for LaTeX table generation in reports.
     """
     config = load_yaml_config(config_file)
 
