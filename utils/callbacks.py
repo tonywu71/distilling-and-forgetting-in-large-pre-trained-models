76945a98c4de076b33e47d72a90f833c277af9d3
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-05 14:44:03 +0100

    Fix merge conflict in previous rebase

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -93,32 +93,28 @@ class WandbCustomCallback(WandbCallback):
             # in the data collator to ignore padded tokens correctly during decoding:
             label_ids[label_ids==PADDING_IDX] = self.processor.tokenizer.pad_token_id  # type: ignore
             
-            # Decode the predictions:
-            curr_pred_str_raw = self.processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=False, normalize=False)[0]  # type: ignore
-            curr_pred_str_raw = "".join(curr_pred_str_raw)
+            
+            # Decode both the predictions and the labels:
+            curr_label_str = self.processor.tokenizer.batch_decode(label_ids, skip_special_tokens=False, normalize=True)[0]  # type: ignore
+            curr_label_str = "".join(curr_label_str)
             
             curr_pred_str = self.processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=True, normalize=True)[0]  # type: ignore
             curr_pred_str = "".join(curr_pred_str)
             
-            self.records["pred_str_raw"].append(curr_pred_str_raw)
+            self.records["label_str"].append(curr_label_str)
             self.records["pred_str"].append(curr_pred_str)
             
-            # Decode the labels:
-            curr_label_str = self.processor.tokenizer.batch_decode(label_ids, skip_special_tokens=True, normalize=True)[0]  # type: ignore
-            curr_label_str = "".join(curr_label_str)
-            
-            self.records["label_str_raw"].append(curr_label_str_raw)
-            self.records["label_str"].append(curr_label_str)
             
             # Decode the predictions and labels without removing special tokens:
             if self.log_raw_str:
-                curr_pred_str_raw = self.processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=False, normalize=True)[0]  # type: ignore
-                curr_pred_str_raw = "".join(curr_pred_str_raw)
-                records["pred_str_raw"].append(curr_pred_str_raw)
-                
                 curr_label_str_raw = self.processor.tokenizer.batch_decode(label_ids, skip_special_tokens=False, normalize=False)[0]  # type: ignore
                 curr_label_str_raw = "".join(curr_label_str_raw)
-                records["label_str_raw"].append(curr_label_str_raw)
+                self.records["label_str_raw"].append(curr_label_str_raw)
+                
+                curr_pred_str_raw = self.processor.tokenizer.batch_decode(pred_ids, skip_special_tokens=False, normalize=True)[0]  # type: ignore
+                curr_pred_str_raw = "".join(curr_pred_str_raw)
+                self.records["pred_str_raw"].append(curr_pred_str_raw)
+            
             
             # Compute the WER:
             self.records["wer"].append(100 * self.wer_metric.compute(references=[curr_label_str], predictions=[curr_pred_str]))  # type: ignore
