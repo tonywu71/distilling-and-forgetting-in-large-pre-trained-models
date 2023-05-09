42feffa6e2da3608ba8055282af371a7682e6872
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-09 17:23:34 +0100

    Add plot_util for relative WER

--- scripts/report_utils/plot_forgetting_wrt_checkpoints_on_fab.py
+++ scripts/report_utils/plot_forgetting_wrt_checkpoints_on_fab.py
@@ -16,7 +16,7 @@ from utils.constants import DEFAULT_OUTPUT_DIR
 sns.set_theme(context="paper", style="ticks")
 
 
-def main(filepath: str):
+def main(filepath: str, is_relative: bool=False):
     """
     Script that takes a CSV output from `compare_multiple_models_to_latex.py` and saves
     a plot of the evolution of WER with respect to fine-tuning checkpoints on the
@@ -31,13 +31,21 @@ def main(filepath: str):
     df = df.T
     df.index = df.index.str.extract(r'checkpoint-(\d+)-fab').astype(int).values.flatten()  # type: ignore
     
+    if is_relative:
+        df = 100 * (df - df.iloc[0]) / df.iloc[0]
+    
     plt.figure(figsize=(6, 4))
     sns.lineplot(df.iloc[:, :-1], marker="o", dashes=False)
+    if is_relative:
+        plt.axhline(0, color="black", linestyle="--")
     plt.xlabel("Steps")
-    plt.ylabel("WER (%)")
+    plt.ylabel("WER (%)") if not is_relative else plt.ylabel("Relative WER difference (%)")
     
     # Save figure:
-    savepath = (DEFAULT_OUTPUT_DIR / "compare_multiple_models" / Path(filepath).stem).with_suffix(".png")
+    if is_relative:
+        savepath = (DEFAULT_OUTPUT_DIR / "compare_multiple_models" / (Path(filepath).stem + "-relative")).with_suffix(".png")
+    else:
+        savepath = (DEFAULT_OUTPUT_DIR / "compare_multiple_models" / Path(filepath).stem).with_suffix(".png")
     savepath.parent.mkdir(parents=True, exist_ok=True)
     plt.tight_layout()
     plt.savefig(savepath)
