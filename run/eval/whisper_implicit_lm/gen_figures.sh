95bcb2ca39019cd2d8615030ac8a24d9a05ad3c2
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-13 17:13:39 +0100

    Adapt plot util for perplexity

---
+++ run/eval/whisper_implicit_lm/gen_figures.sh
@@ -0,0 +1,17 @@
+# Generate CSV from eval outputs:
+python scripts/report_utils/compare_multiple_models_to_csv.py \
+	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-500-implicit_lm-perplexity-fab.csv \
+	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-1000-implicit_lm-perplexity-fab.csv \
+	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-1500-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-2000-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-2500-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3000-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3500-implicit_lm-perplexity-fab.csv
+
+
+# Generate plot from previous CSV:
+python scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
+    outputs/compare_multiple_models/perplexity/perplexity_wrt_checkpoints-fab.csv
+
+python scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
+    outputs/compare_perplexity_multiple_models/perplexity_wrt_checkpoints-fab.csv --is-relative
