d909681502eeb1ed960284a81ad569e467a4d352
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-13 17:13:39 +0100

    Adapt plot util for perplexity

--- scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py
+++ scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py
@@ -18,18 +18,15 @@ sns.set_theme(context="paper", style="ticks")
 
 def main(filepath: str, is_relative: bool=False):
     """
-    Script that takes a CSV output from `compare_multiple_models_to_latex.py` and saves
-    a plot of the evolution of WER with respect to fine-tuning checkpoints on the
+    Script that takes a CSV output from `compare_multiple_models_to_csv.py` and saves
+    a plot of the evolution of perplexity with respect to fine-tuning checkpoints on the
     FAB dataset.
-    
-    Used to generate the table in the "Impact of over-training on forgetting" section
-    of the April Report.
     """
     
     df = pd.read_csv(filepath).set_index("Dataset")
     df.index.name = "FAB dataset"
     df = df.T
-    df.index = df.index.str.extract(r'checkpoint-(\d+)-fab').astype(int).values.flatten()  # type: ignore
+    df.index = df.index.str.extract(r'checkpoint-(\d+)-implicit_lm-perplexity-fab').astype(int).values.flatten()  # type: ignore
     
     if is_relative:
         df = 100 * (df - df.iloc[0]) / df.iloc[0]
@@ -39,13 +36,13 @@ def main(filepath: str, is_relative: bool=False):
     if is_relative:
         plt.axhline(0, color="black", linestyle="--")
     plt.xlabel("Steps")
-    plt.ylabel("WER (%)") if not is_relative else plt.ylabel("Relative WER difference (%)")
+    plt.ylabel("Perplexity") if not is_relative else plt.ylabel("Relative perplexity difference (%)")
     
     # Save figure:
     if is_relative:
-        savepath = (DEFAULT_OUTPUT_DIR / "compare_multiple_models" / (Path(filepath).stem + "-relative")).with_suffix(".png")
+        savepath = (DEFAULT_OUTPUT_DIR / "compare_perplexity_multiple_models" / (Path(filepath).stem + "-relative")).with_suffix(".png")
     else:
-        savepath = (DEFAULT_OUTPUT_DIR / "compare_multiple_models" / Path(filepath).stem).with_suffix(".png")
+        savepath = (DEFAULT_OUTPUT_DIR / "compare_perplexity_multiple_models" / Path(filepath).stem).with_suffix(".png")
     savepath.parent.mkdir(parents=True, exist_ok=True)
     plt.tight_layout()
     plt.savefig(savepath)
