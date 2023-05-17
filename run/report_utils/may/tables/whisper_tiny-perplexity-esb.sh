6385e2904f66d7e99bef5eec6b8b859faef9431e
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-17 17:12:19 +0100

    Various changes for report utils: - Add option to get extra statistics in `compare_2_models_to_latex.py` - Reorganize `scripts/report_utils` - Reorganize `run/report_utils` - Reorganize `run/cache` - TODO: Add post-processing for MLS

---
+++ run/report_utils/may/tables/whisper_tiny-perplexity-esb.sh
@@ -0,0 +1,4 @@
+python scripts/report_utils/compare_2_models/compare_2_models_to_latex.py \
+    "outputs/finetuning/vanilla/whisper-tiny-esb.csv" \
+    "outputs/finetuning/vanilla/whisper-tiny-implicit_lm-perplexity-esb.csv" \
+    --dataset-group esb
---
+++ run/report_utils/may/tables/whisper_tiny-perplexity-mls.sh
@@ -0,0 +1,4 @@
+python scripts/report_utils/compare_2_models/compare_2_models_to_latex.py \
+    "outputs/finetuning/vanilla/whisper-tiny-mls.csv" \
+    "outputs/finetuning/vanilla/whisper-tiny-implicit_lm-perplexity-mls.csv" \
+    --dataset-group mls
