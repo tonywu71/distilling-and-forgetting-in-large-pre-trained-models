225a72e5e54705f668e0d3755c75d966a47f6545
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-17 17:22:24 +0100

    Fix wrong filepaths in `report_utils` scrips

--- run/report_utils/april/tables/whisper_tiny-wer-mls.sh
+++
@@ -1,4 +0,0 @@
-python scripts/report_utils/compare_2_models/compare_2_models_to_latex.py \
-    "outputs/finetuning/vanilla/whisper-tiny-esb.csv" \
-    "outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/checkpoint-3500-esb.csv" \
-    --dataset-group mls
--- run/report_utils/may/tables/whisper_tiny-perplexity-mls.sh
+++ run/report_utils/may/tables/whisper_tiny-perplexity-mls.sh
@@ -1,4 +1,4 @@
 python scripts/report_utils/compare_2_models/compare_2_models_to_latex.py \
-    "outputs/finetuning/vanilla/whisper-tiny-mls.csv" \
     "outputs/finetuning/vanilla/whisper-tiny-implicit_lm-perplexity-mls.csv" \
+    "outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3500-implicit_lm-perplexity-mls.csv" \
     --dataset-group mls
