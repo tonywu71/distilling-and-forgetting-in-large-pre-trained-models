118ba367d7a93be4a33d87605060556c9cd4486a
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-22 23:07:46 +0100

    Update scripts to gen report figs

--- run/report_utils/may/figs/plot_wer_wrt_ppl-english-esb.sh
+++ run/report_utils/may/figs/plot_wer_wrt_ppl-english-esb.sh
@@ -1,36 +1,32 @@
 # ======= 1. Generate CSV from eval outputs =======
 
-# --- Multilingual ---
+
+# --- English models ---
 # tiny:
-python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
-    outputs/vanilla/tiny/whisper-tiny-esb.csv \
-    outputs/vanilla/tiny/whisper-tiny-implicit_lm-perplexity-esb.csv \
-    tiny_multilingual-vanilla-esb
-
-python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/checkpoint-3500-esb.csv \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3500-implicit_lm-perplexity-esb.csv \
-    tiny_multilingual-finetuned-esb
-
-# medium:
-python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
-    outputs/vanilla/medium/whisper-medium-esb.csv \
-    outputs/vanilla/medium/whisper-medium-implicit_lm-perplexity-esb.csv \
-    medium_multilingual-vanilla-esb
-
-
-
-# --- English ---
 python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
     outputs/vanilla/tiny-en/whisper-tiny-en-esb.csv \
     outputs/vanilla/tiny-en/whisper-tiny-en-implicit_lm-perplexity-esb.csv \
     tiny_english-vanilla-esb
 
+# tiny finetuned:
 python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
     outputs/finetuning/english_model/finetune-whisper_tiny_en-librispeech_clean_100h-freeze_encoder/checkpoint-2500-esb.csv \
     outputs/finetuning/english_model/finetune-whisper_tiny_en-librispeech_clean_100h-freeze_encoder/checkpoint-2500-implicit_lm-perplexity-esb.csv \
     tiny_english-finetuned-esb
 
+# base:
+python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
+    outputs/vanilla/base-en/whisper-base-en-esb.csv \
+    outputs/vanilla/base-en/whisper-base-en-implicit_lm-perplexity-esb.csv \
+    base_english-vanilla-esb
+
+# small:
+python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
+    outputs/vanilla/small-en/whisper-small-en-esb.csv \
+    outputs/vanilla/small-en/whisper-small-en-implicit_lm-perplexity-esb.csv \
+    small_english-vanilla-esb
+
+# medium:
 python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py \
     outputs/vanilla/medium-en/whisper-medium-en-esb.csv \
     outputs/vanilla/medium-en/whisper-medium-en-implicit_lm-perplexity-esb.csv \
@@ -40,15 +36,7 @@ python scripts/report_utils/plot_wer_wrt_perplexity/merge_wer_and_ppl_to_csv.py 
 
 # ======= 2. Plot =======
 
-# DEPRECATED:
-# python scripts/report_utils/plot_wer_wrt_perplexity/plot_wer_wrt_ppl.py \
-#     outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-tiny_multilingual-vanilla-esb.csv \
-#     outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-tiny_multilingual-finetuned-esb.csv \
-#     --kind regression \
-#     --filename tiny_multilingual_esb
-
-
-# --- PPL vs WER - impact of fine-tuning on Whisper tiny ---
+# --- PPL vs WER - impact of fine-tuning on Whisper tiny-en ---
 python scripts/report_utils/plot_wer_wrt_perplexity/plot_wer_wrt_ppl.py \
     outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-tiny_english-vanilla-esb.csv \
     outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-tiny_english-finetuned-esb.csv \
@@ -58,3 +46,11 @@ python scripts/report_utils/plot_wer_wrt_perplexity/plot_wer_wrt_ppl.py \
     --ylim 0 \
     --ylim 50 \
     --filename tiny_english_esb
+
+python scripts/report_utils/plot_wer_wrt_perplexity/plot_wer_wrt_ppl.py \
+    outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-tiny_english-vanilla-esb.csv \
+    outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-base_english-vanilla-esb.csv \
+    outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-small_english-vanilla-esb.csv \
+    outputs/report/plot_wer_wrt_perplexity/esb/wer_and_ppl-medium_english-vanilla-esb.csv \
+    --kind regression \
+    --filename all_english_esb
