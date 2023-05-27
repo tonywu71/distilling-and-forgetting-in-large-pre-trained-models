225a3a574b712afb08c2ce88619accebbd6c42d1
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-27 09:49:39 +0100

    Add BaseDatasetGroup for AMI: - Implement `AmiTestSet` - Changes for `LibriSpeechCleanTestSet` - Update eval scripts

---
+++ dataloader/datasets/ami_test.py
@@ -0,0 +1,48 @@
+from typing import Optional, List
+
+import os
+
+from datasets import load_dataset, concatenate_datasets
+
+from dataloader.datasets.base_dataset_group import BaseDatasetGroup
+from dataloader.dataloader_custom.dataloader_ami import LIST_SUBSETS_AMI
+
+
+class AmiTestSet(BaseDatasetGroup):
+    """
+    Util DatasetGroup to eval the vanilla Whisper model on the AMI set.
+    """
+    
+    def __init__(self,
+                 streaming: bool=False,
+                 subset: Optional[List[str]]=None) -> None:
+        
+        self.available_datasets = [
+            "librispeech_clean_validation"
+        ]
+        
+        self.is_multilingual = False
+        self.language = "english"
+        
+        # Retrieve custom `cache_dir` filepath if set:
+        self.cache_dir_ami = os.environ.get("CACHE_DIR_AMI", None)
+        if self.cache_dir_ami is None:
+            print("WARNING: `CACHE_DIR_AMI` environment variable not set. Using default cache directory.")
+        else:
+            print(f"Using cache directory: `{self.cache_dir_ami}`.")
+        
+        super().__init__(streaming=streaming, subset=subset)
+    
+    
+    def _prepare_str2dataset(self) -> None:
+        list_ds = []
+        for subset in LIST_SUBSETS_AMI:
+            list_ds.append(load_dataset("edinburghcstr/ami",
+                                        name=subset,
+                                        split="test",
+                                        streaming=self.streaming,
+                                        cache_dir=self.cache_dir_ami))
+        
+        self.str2dataset = {
+            "ami_test": concatenate_datasets(list_ds)
+        }
--- scripts/eval_whisper/eval_whisper_on_ami_test.py
+++ scripts/eval_whisper/eval_whisper_on_ami_test.py
@@ -1,10 +1,11 @@
-from pathlib import Path
-from pprint import pprint
 import typer
 
 import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from pathlib import Path
+from pprint import pprint
 
 import torch
 assert torch.cuda.is_available(), "This script requires a GPU."
@@ -16,7 +17,7 @@ from typing import List, Optional
 
 import wandb
 
-from dataloader.datasets.librispeech_clean_validation import LibriSpeechCleanValidation
+from dataloader.datasets.ami_test import AmiTestSet
 from evaluation.eval_whisper_on_dataset import eval_whisper_on_dataset
 from utils.file_io import extract_experiment_name, extract_savepath
 
@@ -24,22 +25,22 @@ from utils.file_io import extract_experiment_name, extract_savepath
 
 def main(pretrained_model_name_or_path: str=typer.Argument(..., help="Path to the pretrained model or its name in the HuggingFace Hub."),
          streaming: bool=typer.Option(False, help="Whether to use streaming inference."),
-         subset: Optional[List[str]]=typer.Option(None, help="Subset of the LibriSpeech clean validation dataset to evaluate on."),
          batch_size: int=typer.Option(16, help="Batch size for the ASR pipeline."),
          savepath: Optional[str]=typer.Option(
              None, help="Filename of the output CSV file. Leave to `None` to use the name of `pretrained_model_name_or_path` as the filename.")) -> None:
     """
-    Evaluate the whisper model on the LibriSpeech clean validation set.
+    Evaluate the whisper model on the LibriSpeech clean test set.
     Note that only greedy decoding is supported for now.
     """
     
     # Set up the parameters:
     task = "transcribe"
+    subset = None  # there is only one dataset in this dataset group so we don't need to specify a subset
     
     config = {
         "pretrained_model_name_or_path": pretrained_model_name_or_path,
         "task": task,
-        "dataset": "fab",
+        "dataset": "ami",
         "streaming": streaming,
         "subset": subset,
         "batch_size": batch_size,
@@ -53,27 +54,27 @@ def main(pretrained_model_name_or_path: str=typer.Argument(..., help="Path to th
     wandb.login()
     wandb.init(project=os.environ["WANDB_PROJECT"],
                job_type="evaluation",
-               name=f"eval_librispeech_clean_val-{extract_experiment_name(pretrained_model_name_or_path)}",
+               name=f"eval_librispeech_clean_test-{extract_experiment_name(pretrained_model_name_or_path)}",
                config=config)
     
     
     # Load dataset:
     if subset:
-        print(f"Subset(s) of FAB: {subset}")
+        print(f"Subset(s) of AMI: {subset}")
         
-    librispeech_dataset = LibriSpeechCleanValidation(streaming=streaming, subset=subset)
-    print(f"Loaded datasets: {list(librispeech_dataset.keys())}")
+    ami_dataset = AmiTestSet(streaming=streaming, subset=subset)
+    print(f"Loaded datasets: {list(ami_dataset.keys())}")
     
     
     # Preprocess:
     print("Preprocessing datasets...")
-    librispeech_dataset.preprocess_datasets(normalize=True)
+    ami_dataset.preprocess_datasets(normalize=True)
     
     
     # Evaluate:
     print("Evaluating...")
     results = eval_whisper_on_dataset(pretrained_model_name_or_path=pretrained_model_name_or_path,
-                                      ds_group=librispeech_dataset,
+                                      ds_group=ami_dataset,
                                       batch_size=batch_size,
                                       task=task)
     
@@ -86,7 +87,7 @@ def main(pretrained_model_name_or_path: str=typer.Argument(..., help="Path to th
     
     # Save results:
     if savepath is None:
-        savepath = extract_savepath(pretrained_model_name_or_path)  + "-fab.csv"
+        savepath = extract_savepath(pretrained_model_name_or_path)  + "-ami.csv"
     
     Path(savepath).parent.mkdir(exist_ok=True, parents=True)
     results.to_csv(f"{savepath}")
