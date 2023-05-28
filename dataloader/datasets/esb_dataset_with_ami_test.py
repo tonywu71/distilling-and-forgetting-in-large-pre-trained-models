80be7f3fae0790cf2d07f1f507614b5dd446b48c
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-28 16:47:51 +0100

    Fix wrong DatasetGroup creation

--- dataloader/datasets/esb_dataset_with_ami_test.py
+++ dataloader/datasets/esb_dataset_with_ami_test.py
@@ -3,11 +3,11 @@ import os
 from typing import Optional, List
 from datasets import load_dataset, concatenate_datasets
 
-from dataloader.datasets.esb_dataset import ESBDataset
+from dataloader.datasets.base_dataset_group import BaseDatasetGroup
 from dataloader.dataloader_custom.dataloader_ami import LIST_SUBSETS_AMI
 
 
-class ESBDatasetWithAMITest(ESBDataset):
+class ESBDatasetWithAMITest(BaseDatasetGroup):
     """
     ESB dataset group with the LibriSpeech dataset replaced with
     the test split of the original AMI dataset.
@@ -18,10 +18,37 @@ class ESBDatasetWithAMITest(ESBDataset):
                  load_diagnostic: bool=True,
                  subset: Optional[List[str]]=None) -> None:
         
-        super().__init__(streaming=streaming, load_diagnostic=load_diagnostic, subset=subset)
+        self.dataset_path = "esb/datasets" if not load_diagnostic else "esb/diagnostic-dataset"
+        self.available_datasets = [
+            "librispeech",
+            "common_voice",
+            "voxpopuli",
+            "tedlium",
+            "gigaspeech",
+            "spgispeech",
+            "earnings22",
+            "ami"
+        ]
+        self.is_multilingual = False
+        self.language = "english"
+        
+        self.load_diagnostic = load_diagnostic
         
         
         # Retrieve custom `cache_dir` filepath if set:
+        if self.load_diagnostic:
+            self.cache_dir_esb = os.environ.get("CACHE_DIR_ESB_DIAGNOSTIC", None)
+            if self.cache_dir_esb is None:
+                print("WARNING: `CACHE_DIR_ESB_DIAGNOSTIC` environment variable not set. Using default cache directory.")
+            else:
+                print(f"Using cache directory: `{self.cache_dir_esb}`.")
+        else:
+            self.cache_dir_esb = os.environ.get("CACHE_DIR_ESB", None)
+            if self.cache_dir_esb is None:
+                print("WARNING: `CACHE_DIR_ESB` environment variable not set. Using default cache directory.")
+            else:
+                print(f"Using cache directory: `{self.cache_dir_esb}`.")
+        
         self.cache_dir_ami = os.environ.get("CACHE_DIR_AMI", None)
         if self.cache_dir_ami is None:
             print("WARNING: `CACHE_DIR_AMI` environment variable not set. Using default cache directory.")
@@ -39,6 +66,8 @@ class ESBDatasetWithAMITest(ESBDataset):
             "earnings22": self.cache_dir_esb,
             "ami": self.cache_dir_ami,
         }
+        
+        super().__init__(streaming=streaming, subset=subset)
     
     
     def _prepare_str2dataset(self) -> None:
--- dataloader/datasets/esb_dataset_with_librispeech_test.py
+++ dataloader/datasets/esb_dataset_with_librispeech_test.py
@@ -3,10 +3,10 @@ import os
 from typing import Optional, List
 from datasets import load_dataset
 
-from dataloader.datasets.esb_dataset import ESBDataset
+from dataloader.datasets.base_dataset_group import BaseDatasetGroup
 
 
-class ESBDatasetWithLibriSpeechTest(ESBDataset):
+class ESBDatasetWithLibriSpeechTest(BaseDatasetGroup):
     """
     ESB dataset group with the LibriSpeech dataset replaced with
     the test splits of the original LibriSpeech dataset.
@@ -17,10 +17,38 @@ class ESBDatasetWithLibriSpeechTest(ESBDataset):
                  load_diagnostic: bool=True,
                  subset: Optional[List[str]]=None) -> None:
         
-        super().__init__(streaming=streaming, load_diagnostic=load_diagnostic, subset=subset)
+        self.dataset_path = "esb/datasets" if not load_diagnostic else "esb/diagnostic-dataset"
+        self.available_datasets = [
+            "librispeech",
+            "common_voice",
+            "voxpopuli",
+            "tedlium",
+            "gigaspeech",
+            "spgispeech",
+            "earnings22",
+            "ami"
+        ]
+        self.is_multilingual = False
+        self.language = "english"
+        
+        self.load_diagnostic = load_diagnostic
         
         
         # Retrieve custom `cache_dir` filepath if set:
+        if self.load_diagnostic:
+            self.cache_dir_esb = os.environ.get("CACHE_DIR_ESB_DIAGNOSTIC", None)
+            if self.cache_dir_esb is None:
+                print("WARNING: `CACHE_DIR_ESB_DIAGNOSTIC` environment variable not set. Using default cache directory.")
+            else:
+                print(f"Using cache directory: `{self.cache_dir_esb}`.")
+        else:
+            self.cache_dir_esb = os.environ.get("CACHE_DIR_ESB", None)
+            if self.cache_dir_esb is None:
+                print("WARNING: `CACHE_DIR_ESB` environment variable not set. Using default cache directory.")
+            else:
+                print(f"Using cache directory: `{self.cache_dir_esb}`.")
+        
+        
         self.cache_dir_librispeech = os.environ.get("CACHE_DIR_LIBRISPEECH", None)
         if self.cache_dir_librispeech is None:
             print("WARNING: `CACHE_DIR_LIBRISPEECH` environment variable not set. Using default cache directory.")
@@ -38,6 +66,8 @@ class ESBDatasetWithLibriSpeechTest(ESBDataset):
             "earnings22": self.cache_dir_esb,
             "ami": self.cache_dir_esb,
         }
+        
+        super().__init__(streaming=streaming, subset=subset)
     
     
     def _prepare_str2dataset(self) -> None:
