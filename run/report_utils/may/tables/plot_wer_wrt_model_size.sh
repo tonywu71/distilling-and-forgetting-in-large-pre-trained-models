81a730ea6be80bcca9da65a17517d25adea5a4f5
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-29 15:22:06 +0100

    Fix original wer_wrt_model_size figure

--- run/report_utils/may/tables/plot_wer_wrt_model_size.sh
+++ run/report_utils/may/tables/plot_wer_wrt_model_size.sh
@@ -1,4 +1,7 @@
-# --- BASE ---
+python scripts/report_utils/wer_wrt_model_size/plot_wer_wrt_model_size.py \
+    outputs/report/wer_wrt_model_size/wer_wrt_model_size-esb_librispeech.csv \
+    --savename wer_wrt_model_size-esb_librispeech
+
 python scripts/report_utils/wer_wrt_model_size/plot_wer_wrt_model_size.py \
     outputs/report/wer_wrt_model_size/wer_wrt_model_size-ami.csv \
     --plot-ideal \
