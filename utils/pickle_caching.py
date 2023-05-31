6b906df6c6cc6ba711ad85ce3a33f43cb5a9cea3
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-05-31 11:13:59 +0100

    Add pickle utils

---
+++ utils/pickle_caching.py
@@ -0,0 +1,19 @@
+from typing import Any
+from pathlib import Path
+import pickle
+
+
+def save_as_pickle(data: Any, savepath: str) -> None:
+    """
+    Save data as pickle file.
+    If the parent directory does not exist, it will be created before saving.
+    """
+    if not Path(savepath).parent.exists():
+        Path(savepath).parent.mkdir(parents=True)
+    with open(savepath, 'wb') as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_pickle(path: str) -> Any:
+    with open(path, 'rb') as f:
+        return pickle.load(f)
