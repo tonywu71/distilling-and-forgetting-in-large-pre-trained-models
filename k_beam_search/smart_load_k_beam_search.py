24bb501fc40065b314ce9d396753e4c3588695b2
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-01 12:26:19 +0100

    Add support for using higher k_cached

--- k_beam_search/smart_load_k_beam_search.py
+++ k_beam_search/smart_load_k_beam_search.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import os
 import shutil
+import re
 from pathlib import Path
 
 from transformers.generation import BeamSearchEncoderDecoderOutput
@@ -36,13 +37,34 @@ def smart_load_k_beam_search(config: DistilConfig,
     teacher_ref = extract_exp_name_from_model_path(config.teacher_model_name_or_path)
     
     # Get the path to the preprocessed dataset:
-    dataset_dir = str(Path(os.environ["PREPROCESSED_DATASETS_DIR"]) / K_BEAM_SEARCH_DIRNAME / \
-                      config.dataset_name / teacher_ref / f"k_{config.distillation_num_beams}")
-    # Example of generated `dataset_dir` -> "preprocessed_datasets/k_beam_search/ami_10h/openai/whisper-medium/k_5/"
+    dataset_dir_all = Path(os.environ["PREPROCESSED_DATASETS_DIR"]) \
+                        / K_BEAM_SEARCH_DIRNAME / config.dataset_name / teacher_ref
+    
+    
+    list_suitable_cached_k = []
+    
+    # Get list of all subdirectories in `dataset_dir_all`:
+    for x in dataset_dir_all.iterdir():
+        if x.is_dir():
+            # Use Regex to retrieve the number of beams from the directory name:
+            # Example: "preprocessed_datasets/k_beam_search/ami_10h/openai/whisper-medium/k_5/" -> "5"
+            reg_pattern = r'^k_\d+$'
+            if re.match(reg_pattern, x.name):
+                cached_k = int(re.findall(reg_pattern, x.name)[0])
+                if cached_k >= config.distillation_num_beams:
+                    list_suitable_cached_k.append(cached_k)
+    
+    # Use the cached K-Beam search results with the smallest K
+    # that is >= `config.distillation_num_beams` for efficiency:
+    dataset_dir = str(dataset_dir_all / f"k_{min(list_suitable_cached_k)}") if list_suitable_cached_k else None
+    
+    # Example of generated `dataset_dir`
+    # - "preprocessed_datasets/k_beam_search/ami_10h/openai/whisper-medium/k_5/" if there is a suitable cache
+    # - None otherwise
     
     
     # Load the preprocessed dataset if it exists and if `force_reprocess_k_best` is set to `False`:
-    if not config.force_reprocess_k_best and Path(dataset_dir).exists():
+    if not config.force_reprocess_k_best and dataset_dir is not None:
         print(f"Previously saved K-Beam search results found at `{dataset_dir}`. Loading from disk...")
         id_to_k_beam_search_output = load_pickle(dataset_dir)
     
@@ -51,7 +73,7 @@ def smart_load_k_beam_search(config: DistilConfig,
     else:
         if config.force_reprocess_k_best:
             print(f"`force_reprocess_k_best` was set to `True` in the config file. ")
-            if Path(dataset_dir).exists():
+            if dataset_dir is not None:
                 print(f"Deleting previously saved K-Beam search results at `{dataset_dir}`...")
                 shutil.rmtree(dataset_dir) 
         else:
@@ -64,6 +86,12 @@ def smart_load_k_beam_search(config: DistilConfig,
                                                               col_id=DATASET_NAME_TO_COL_ID[config.dataset_name],
                                                               num_beams=config.distillation_num_beams)
         
+        # If we need to cache the K-Beam search results from scratch, K must be >= 2.
+        # This is not a problem as we will simply use K=1 (≤ 2) for the distillation.
+        k_beams = min(config.distillation_num_beams, 2)
+
+        # Set the path to save the K-Beam search results:
+        dataset_dir = str(dataset_dir_all / f"k_{k_beams}")
         
         Path(dataset_dir).mkdir(parents=True, exist_ok=True)
         save_as_pickle(id_to_k_beam_search_output, savepath=dataset_dir)
