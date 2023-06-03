Validate new sequence-level loss computation: - Fix multiple issues for the loss computation function - Add `get_attention_mask_from_tensor` - Add optional use of loss mask in DataCollator - Fix wrong k-beam cache loading - Set `remove_unused_columns=False` in Trainer - Fix wrong processor used in the distil script - Fix a few typos

dataloader/collator.py
@@ -13,9 +13,9 @@ class DataCollatorSpeechSeq2SeqWithPadding:
     
     def __init__(self,
                  processor: WhisperProcessor,
-                 is_distillation: bool=False):
+                 add_k_beam_features: bool=False):
         self.processor = processor
-        self.is_distillation = is_distillation
+        self.add_k_beam_features = add_k_beam_features
     
     
     def __call__(self,
@@ -30,7 +30,6 @@ class DataCollatorSpeechSeq2SeqWithPadding:
                 "labels": [...],
                 "input_ids": [...]
             },
-            
             ...
         ]
         
@@ -41,33 +40,49 @@ class DataCollatorSpeechSeq2SeqWithPadding:
         }
         """
         
+        # features -> list of `batch_size` dicts (each key is a column name)
+        
         # --- Input features ---
-        # Get the input features and apply padding:
-        input_features = [{"input_features": feature["input_features"]} for feature in features]
+        input_features = [{"input_features": feature["input_features"]} for feature in features]  # get only the feature of interest
         batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")  # type: ignore
         
         
         # --- Labels (tokenized) ---
-        labels = self.preprocess_tokenized_labels(features, col_name=DEFAULT_LABEL_TOKENIZED_COL)
+        label_features = [{"input_ids": feature[DEFAULT_LABEL_TOKENIZED_COL]} for feature in features]  # get only the feature of interest
+        labels = self.preprocess_tokenized_labels(label_features, use_loss_mask=True, discard_first_bos_token=True)
         batch[DEFAULT_LABEL_TOKENIZED_COL] = labels
         
         
         # Add K-beam features if distillation:
-        if self.is_distillation:
+        if self.add_k_beam_features:
             # --- Teacher sequences ---
-            labels = self.preprocess_tokenized_labels(features, col_name="teacher_sequences")
+            # Note: `tokenizer.pad` only accepts 1D-tensors which is not the case here as here they have shape (num_beams, n_tokens).
+            #       However, we can take advantage of the fact that batch and beam dimensions are indifferent. Hence, we can simply
+            #       iterate over the batch dimension and pad each tensor individually.
+            batch_size = len(features)
+            teacher_sequences_features = []
+            for feature in features:
+                for row in feature["teacher_sequences"]:
+                    teacher_sequences_features.append({"input_ids": row})  # get only the feature of interest
+            
+            # Important: We should not use the loss mask here as `teacher_sequences_features` will only be used as the reference sequence and
+            #            thus cannot contain the special token `LOSS_MASK_IDX`.
+            labels = self.preprocess_tokenized_labels(teacher_sequences_features, use_loss_mask=False, discard_first_bos_token=False)  # (batch_size * num_beams, n_tokens)
+            
+            labels = labels.reshape(batch_size, -1, labels.shape[-1])  # (batch_size, num_beams, n_tokens)
             batch["teacher_sequences"] = labels
             
             # --- Teacher sequences scores ---
             # No need to pad the scores as they are already of the same shape:
-            batch["teacher_sequences_scores"] = torch.stack([torch.tensor(feature["teacher_sequences_scores"], dim=0) for feature in features])
+            batch["teacher_sequences_scores"] = torch.stack([feature["teacher_sequences_scores"] for feature in features], dim=0)  # (batch_size, num_beams)
         
         return batch
     
     
     def preprocess_tokenized_labels(self,
                                     features: List[Dict[str, Union[List[int], torch.Tensor]]],
-                                    col_name: str):
+                                    use_loss_mask: bool=True,
+                                    discard_first_bos_token: bool=True) -> torch.Tensor:
         """
         Tokenize, pad, and replace padding with correct token for correct loss computation.
         
@@ -75,17 +90,21 @@ class DataCollatorSpeechSeq2SeqWithPadding:
               PADDING_IDX as a way to reconstruct the attention mask during loss computation.
         """
         
-        # Get the tokenized label sequences and apply padding:
-        label_features = [{"input_ids": feature[col_name]} for feature in features]
-        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")  # type: ignore
-        # Note: `labels_batch` contains the following keys: ["input_ids", "attention_mask"]
+        # Pad the features:
+        labels_batch = self.processor.tokenizer.pad(features, return_tensors="pt")  # type: ignore
         
-        # Replace padding with correct token for correct loss computation:
-        labels = labels_batch["input_ids"].masked_fill(labels_batch["attention_mask"].ne(1), LOSS_MASK_IDX)
+        # Note: The output `labels_batch` contains the following keys: ["input_ids", "attention_mask"].
         
-        # If a BOS ("Beginning Of Sequence") token was appended in previous tokenization step,
-        # discard it as it will get appended later anyway:
-        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():  # type: ignore
-            labels = labels[:, 1:]
+        if use_loss_mask:
+            # Replace padding with correct token for correct loss computation:
+            labels = labels_batch["input_ids"].masked_fill(labels_batch["attention_mask"].ne(1), LOSS_MASK_IDX)
+        else:
+            labels = labels_batch["input_ids"]
+        
+        if discard_first_bos_token:
+            # If a BOS ("Beginning Of Sequence") token was appended in previous tokenization step,
+            # discard it as it will get appended later anyway:
+            if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():  # type: ignore
+                labels = labels[:, 1:]
         
         return labels

k_beam_search/prepare_k_beam_features.py
@@ -13,12 +13,14 @@ def prepare_k_beam_features_fct(batch: Dict[str, Any],
     """
     Utility to create K-Beam features for a dataset. Should be used with `Dataset.map()`.
     
+    Important: The dataset must be converted to PyTorch format first.
+    
     The following new columns are added to the dataset:
     - `sequences` -> (num_beams, n_tokens)
     - `sequences_scores` -> (num_beams,)
     """
     
-    input_features = batch["input_features"]
+    input_features = batch["input_features"].to(device)
 
     # Generate teacher predictions using K-beam search:
     outputs = model.generate(input_features,
@@ -31,12 +33,12 @@ def prepare_k_beam_features_fct(batch: Dict[str, Any],
     # - outputs.sequences -> (batch_size * num_beams, n_tokens)
     # - outputs.sequences_scores -> (batch_size * num_beams,)
     
-    # Add the follwoing fields to the current batch, i.e. a fortiori add columns for the dataset:
+    # Add the following fields to the current batch, i.e. a fortiori add columns for the dataset:
     batch["teacher_sequences"] = list(torch.split(outputs.sequences,
                                                   split_size_or_sections=num_beams,
-                                                  dim=0))  # batch_size tensors of shape (num_beams, n_tokens)
+                                                  dim=0))  # `batch_size` tensors of shape (num_beams, n_tokens)
     batch["teacher_sequences_scores"] = list(torch.split(outputs.sequences_scores,
                                                          split_size_or_sections=num_beams,
-                                                         dim=0))  # batch_size tensors of shape (num_beams,)
+                                                         dim=0))  # `batch_size` tensors of shape (num_beams,)
     
     return batch

trainer/distillation.py
@@ -6,9 +6,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from transformers import TrainingArguments, Trainer, PreTrainedModel
+from transformers import TrainingArguments, Trainer, PreTrainedModel, WhisperTokenizer
 from transformers.modeling_outputs import Seq2SeqLMOutput
-from utils.constants import DEFAULT_LABEL_TOKENIZED_COL, LOSS_MASK_IDX
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -40,10 +39,12 @@ class DistillationTrainer(Trainer):
     Trainer class for distillation. Should be used with `args=DistillationTrainingArguments`.
     """
     def __init__(self,
+                 student_tokenizer: Optional[WhisperTokenizer] = None,
                  teacher_model: Optional[PreTrainedModel] = None,
                  *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
+        self.student_tokenizer = student_tokenizer
         self.teacher_model = teacher_model
         
         # Sanity checks:
@@ -115,20 +116,21 @@ class DistillationTrainer(Trainer):
         Compute the loss for k-best sequence-level distillation where `k = self.args.distillation_num_beams`.
         """
         
-        assert self.id_to_k_beam_search_output is not None, \
-            "The `id_to_k_beam_search_output` must be set for sequence-level distillation."
-        
         # Move inputs to device:
         inputs = inputs.to(device)  # inputs.keys -> ['input_features', 'labels']
          
-        input_features = inputs["input_features"]
+        input_features = inputs["input_features"]  # (batch_size, 80, 3000)
         teacher_sequences = inputs["teacher_sequences"]  # (batch_size, num_beams, n_tokens)
-        teacher_sequences_attention_mask = self.get_attention_mask_from_tensor(teacher_sequences)  # (batch_size, num_beams, n_tokens)
         teacher_sequences_scores = inputs["teacher_sequences_scores"]  # (batch_size, num_beams)
         
         batch_size = input_features.shape[0]
         distillation_num_beams = self.args.distillation_num_beams
         n_tokens = teacher_sequences.shape[-1]
+        
+        # Get attention mask for teacher sequences:
+        # Note that `get_attention_mask_from_tensor` expects a 2D tensor. Thus we will have to temporarily reshape `teacher_sequences`.
+        teacher_sequences_attention_mask = self.get_attention_mask_from_tensor(teacher_sequences.reshape(-1, n_tokens))  # (batch_size * num_beams, n_tokens)
+        teacher_sequences_attention_mask = teacher_sequences_attention_mask.reshape(batch_size, -1, n_tokens)  # (batch_size, num_beams, n_tokens)
         
         assert distillation_num_beams <= teacher_sequences.shape[1], \
             f"The number of beams for distillation must be <= the number of beams used for generation. " \
@@ -136,6 +138,7 @@ class DistillationTrainer(Trainer):
         
         # Retrieve only the beams of interest:
         teacher_sequences = teacher_sequences[:, :distillation_num_beams, :]  # (batch_size, distillation_num_beams, n_tokens)
+        teacher_sequences_attention_mask = teacher_sequences_attention_mask[:, :distillation_num_beams, :]  # (batch_size, distillation_num_beams, n_tokens)
         teacher_sequences_scores = teacher_sequences_scores[:, :distillation_num_beams]  # (batch_size, distillation_num_beams)
         
         # Re-normalize scores using only the K-best sequences by applying a softmax over the beam dimension:
@@ -145,16 +148,21 @@ class DistillationTrainer(Trainer):
         # Important note: Technically, the student model should be able to process the entire batch of size `batch_size * distillation_num_beams`. If
         #                 this is not the case, we need to reduce the batch size accordingly.
         teacher_sequences = teacher_sequences.reshape(batch_size * distillation_num_beams, n_tokens)  # (batch_size * distillation_num_beams, n_tokens)
+        teacher_sequences_attention_mask = teacher_sequences_attention_mask.reshape(batch_size * distillation_num_beams, n_tokens)  # (batch_size * distillation_num_beams, n_tokens)
+        
+        # Repeat input features for the number of beams. The resulting tensor will have shape (batch_size * distillation_num_beams, dim_features).
+        input_features = input_features.repeat_interleave(distillation_num_beams, dim=0)  # (batch_size * distillation_num_beams, 80, 3000)
         
         # Forward pass through student:
         # Note that we excluded the EOT token "<|endoftext|>" as generation is supposed to stop here.
         student_output: Seq2SeqLMOutput = student_model.forward(input_features=input_features,
                                                                 decoder_input_ids=teacher_sequences[:, :-1],
-                                                                labels=teacher_sequences[:, 1:],
                                                                 decoder_attention_mask=teacher_sequences_attention_mask[:, :-1])
         
+        
         # Extract cross-entropy loss and logits from student output:
-        loss_ce = student_output.loss  # mean cross-entropy -> (1,)  # TODO: Check if this is the correct reduction
+        # TODO: Get CE wrt to label and not with respect to teacher label !!!
+        # loss_ce = student_output.loss  # mean cross-entropy -> (1,)
         student_logits = student_output.logits  # (batch_size * distillation_num_beams, n_tokens-1, vocab_size)
         vocab_size = student_logits.shape[-1]
         
@@ -172,7 +180,7 @@ class DistillationTrainer(Trainer):
         #       a sufficient method to ignore the padded values is to set them to 0.
         
         # Repeat attention_mask for the n_vocab dimension:
-        student_log_prob_all_mask = teacher_sequences_attention_mask["attention_mask"][:, :-1, None].expand(-1, -1, vocab_size)
+        student_log_prob_all_mask = teacher_sequences_attention_mask[:, :-1, None].expand(-1, -1, vocab_size)
         output_log_prob_all_masked = student_log_prob_all.masked_fill(student_log_prob_all_mask.ne(1), 0)
         
         # Get log-probabilities for each generation step:
@@ -201,10 +209,10 @@ class DistillationTrainer(Trainer):
         # also take the mean of the sequence log-probabilities to be consistent:
         loss_kd = torch.mean(loss_kd,)  # (1,)
         
-        # Return weighted student loss
-        loss = self.args.ce_alpha * loss_ce + (1. - self.args.ce_alpha) * loss_kd
+        # # Return weighted student loss
+        # loss = self.args.ce_alpha * loss_ce + (1. - self.args.ce_alpha) * loss_kd
         
-        return loss, student_output
+        return loss_kd, student_output
     
     
     def _compute_loss_seq_level_k_best_uniform(self,
@@ -225,12 +233,31 @@ class DistillationTrainer(Trainer):
         return self._compute_loss_seq_level_k_best(student_model, inputs, rank_weighting=True)
     
     
-    @staticmethod
-    def get_attention_mask_from_tensor(tensor: torch.Tensor) -> torch.Tensor:
+    def get_attention_mask_from_tensor(self, tensor: torch.Tensor) -> torch.Tensor:
         """
         Returns the attention mask from a tensor of shape (batch_size, n_tokens).
+        
+        Example:
+        - Input: tensor([[50257.,  50362.,     76.,    1694.,    627.,   50256.],
+                         [50257.,  50362.,  13099.,   50256.,  50256.,   50256.]])
+        
+        - Output: tensor([[1, 1, 1, 1, 1, 1],
+                          [1, 1, 1, 1, 0, 0]])
+        
+        Note: The padding token is the same as the end-of-text (EOT) token. Therefore,
+              we should turn off the attention for all the EOT tokens at the exception
+              of the first EOT token of each row (see example above).
         """
-        return tensor.ne(LOSS_MASK_IDX).float()
+        
+        assert tensor.ndim == 2, \
+            f"The tensor must be 2D. Got {tensor.ndim} dimensions."
+        
+        pad_token_id = self.student_tokenizer.pad_token_id
+        indices = (tensor == pad_token_id).long().argmax(dim=-1)
+        attention_mask = torch.zeros_like(tensor, dtype=torch.long)
+        for idx, row in zip(indices, attention_mask):
+            row[idx+1:] = 1  # ignore the first EOT token of each row
+        return attention_mask
     
     
     @staticmethod