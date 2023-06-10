7fbcced9859306338c3fa390c947daf95b349db5
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-10 14:14:48 +0100

    Fix bug in `get_string_edit_metrics`

--- evaluation/string_edit_metrics.py
+++ evaluation/string_edit_metrics.py
@@ -1,3 +1,4 @@
+from re import sub
 from typing import Dict
 from jiwer import compute_measures
 
@@ -14,18 +15,24 @@ def get_string_edit_metrics(predictions: str, references: str) -> Dict[str, floa
             - ins: number of insertions.
     """
     incorrect = 0
+    substitutions = 0
+    deletions = 0
+    insertions = 0
     total = 0
     
     for prediction, reference in zip(predictions, references):
         measures = compute_measures(reference, prediction)
         incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
+        substitutions += measures["substitutions"]
+        deletions += measures["deletions"]
+        insertions += measures["insertions"]
         total += measures["substitutions"] + measures["deletions"] + measures["hits"]
     
     string_edit_metrics = {
         "wer": incorrect / total,
-        "sub": measures["substitutions"] / total,
-        "del": measures["deletions"] / total,
-        "ins": measures["insertions"] / total
+        "sub": substitutions / total,
+        "del": deletions / total,
+        "ins": insertions / total
     }
     
     return string_edit_metrics
