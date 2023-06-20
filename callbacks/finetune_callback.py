3a49e4bfc199cc83a2ea5a2f5f8f22920bf33649
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-20 23:42:30 +0100

    Fix bug when references are empty

--- callbacks/finetune_callback.py
+++ callbacks/finetune_callback.py
@@ -10,12 +10,11 @@ from transformers import (PreTrainedModel,
                           TrainerState,
                           TrainerControl)
 from datasets import Dataset
-import evaluate
 
-from dataloader.collator import DataCollatorSpeechSeq2SeqWithPadding
 from callbacks.base_training_callback import BaseWandbTrainingCallback
+from dataloader.collator import DataCollatorSpeechSeq2SeqWithPadding
+from evaluation.string_edit_metrics import get_string_edit_metrics
 from utils.finetune_config import FinetuneConfig
-from utils.distil_config import DistilConfig
 from utils.constants import GEN_MAX_LENGTH, LOSS_MASK_IDX, DEFAULT_LABEL_TOKENIZED_COL
 
 
@@ -43,8 +42,6 @@ class WandbFinetuneCallback(BaseWandbTrainingCallback):
         
         self.table_name = "sample_predictions-finetune"
         self.data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=self.processor)
-        
-        self.wer_metric = evaluate.load("wer")
     
     
     def log_records_to_wandb(self) -> None:
@@ -109,7 +106,7 @@ class WandbFinetuneCallback(BaseWandbTrainingCallback):
             curr_pred_str = self.records["pred"][-1]
             
             # Compute the WER:
-            self.records["wer"].append(100 * self.wer_metric.compute(references=[curr_label_str], predictions=[curr_pred_str]))  # type: ignore
+            self.records["wer"].append(100 * get_string_edit_metrics(references=[curr_label_str], predictions=[curr_pred_str])["wer"])
             
             # Add boolean flag to indicate whether the prediction is correct:
             self.records["is_correct"].append(curr_label_str == curr_pred_str)
--- evaluation/string_edit_metrics.py
+++ evaluation/string_edit_metrics.py
@@ -1,8 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 from jiwer import compute_measures
 
 
-def get_string_edit_metrics(predictions: str, references: str) -> Dict[str, float]:
+def get_string_edit_metrics(references: List[str], predictions: List[str]) -> Dict[str, float]:
     """
     Return the string edit metrics (WER, substitutions, deletions, insertions) for the given predictions and references.
     
@@ -20,6 +20,9 @@ def get_string_edit_metrics(predictions: str, references: str) -> Dict[str, floa
     total = 0
     
     for prediction, reference in zip(predictions, references):
+        if not reference:  # If the reference is empty...
+            continue  # skip current iteration because the WER is not defined for empty references
+        
         measures = compute_measures(reference, prediction)
         incorrect += measures["substitutions"] + measures["deletions"] + measures["insertions"]
         substitutions += measures["substitutions"]
