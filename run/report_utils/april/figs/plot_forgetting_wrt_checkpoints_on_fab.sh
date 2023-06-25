a9c75388f5ca47a9374feed063e6d69d6c31fa7d
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-25 23:53:56 +0100

    Tweaks

--- run/report_utils/april/figs/plot_forgetting_wrt_checkpoints_on_fab.sh
+++ run/report_utils/april/figs/plot_forgetting_wrt_checkpoints_on_fab.sh
@@ -1,3 +1,3 @@
-python scripts/report_utils/plot_forgetting/plot_forgetting_wer_wrt_checkpoints_on_fab.py \
+python scripts/report_utils/forgetting/plot_forgetting_wer_wrt_checkpoints_on_fab.py \
     outputs/report/compare_wer_multiple_models/wer_wrt_checkpoints-fab.csv \
     --is-relative
