e852352e3489349d718306ee4d8b495813a18cfa
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-26 14:38:04 +0100

    Add script for `plot_tac_forgetting`

---
+++ run/report_utils/june/figs/plot_tac_forgetting.sh
@@ -0,0 +1,6 @@
+python scripts/report_utils/forgetting/plot_tac_forgetting.py \
+    outputs/report/forgetting_wrt_training_steps/tac/tac_finetune_on_librispeech-forgetting_wrt_training_steps-fab.csv
+
+python scripts/report_utils/forgetting/plot_tac_forgetting.py \
+    outputs/report/forgetting_wrt_training_steps/tac/tac_finetune_on_librispeech-forgetting_wrt_training_steps-fab.csv \
+    --is-relative
