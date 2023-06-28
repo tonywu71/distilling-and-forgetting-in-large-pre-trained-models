6db0dbb00af9cf35f85940f20afac94d0ec90198
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-06-28 23:26:46 +0100

    Add scripts to fix existing cached k-beam dataset

--- scripts/utils/add_missing_special_tokens.py
+++ scripts/utils/add_missing_special_tokens.py
@@ -7,25 +7,22 @@ from utils.initialize import initialize_env
 initialize_env()
 
 from pathlib import Path
-from typing import Dict, Any
-from transformers import WhisperTokenizerFast
+from typing import Dict
+import torch
 from datasets import load_from_disk
 
 
 def main(dirpath: str = typer.Argument(..., help="Path to the dataset directory."),
          savepath: str = typer.Argument(..., help="Path to the save directory.")):
-    tokenizer = WhisperTokenizerFast.from_pretrained("openai/whisper-tiny", language="en", task="transcribe")
     dataset_dict = load_from_disk(dirpath)
 
-    def map_fct(batch: Dict[str, Any]) -> Dict[str, Any]:
-        batch["text"] = batch["text"].lower()
-        # Encode from target text to label ids:
-        batch["labels"] = tokenizer(batch["text"]).input_ids
-        return batch
+    def map_fct(labels: torch.LongTensor) -> Dict[str, torch.LongTensor]:
+        labels = torch.cat([labels[0:1], torch.LongTensor([50259, 50359]), labels[1:]])  # add missing tokens for <EN> and <TRANSCRIBE>
+        return {"labels": labels}
     
     for split in dataset_dict:
         print(f"Preprocessing the {split} set...")
-        dataset_dict[split] = dataset_dict[split].map(map_fct, num_proc=1)
+        dataset_dict[split] = dataset_dict[split].map(map_fct, input_columns=["labels"], num_proc=4)
     
     Path(savepath).parent.mkdir(parents=True, exist_ok=True)
     dataset_dict.save_to_disk(savepath)
