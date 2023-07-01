3ef6b4e65e2335c73ed3d58f17533b71a26ddfc8
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-01 22:56:34 +0100

    Re-organize `report_utils`

--- run/report_utils/june/tables/distillation/compare_wer/compare_wer_distil_methods-ami.sh
+++
@@ -1,6 +0,0 @@
-python scripts/report_utils/compare_wer/compare_wer-multiple_models/compare_wer-multiple_models_1_dataset.py \
-    outputs/vanilla/tiny/whisper-tiny-esb_ami.csv \
-    outputs/vanilla/medium/whisper-medium-esb_ami.csv \
-    checkpoints/distillation/whisper_medium_to_tiny/ami_10h/word_level/checkpoint-250 \
-    # TODO: Insert checkpoints for the remaining models
-    --dataset ami
--- run/report_utils/june/tables/experiments/exp_common_config_to_latex_table.sh
+++
@@ -1,1 +0,0 @@
-python scripts/report_utils/configs/config_to_latex.py configs/distil_configs/experiments/experiments-common_config.yaml --kind other
