5a067c71ec7ea9064c6a7baaa10536648f5a696a
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-01 16:50:49 +0100

    Add wandb to save_ewc_params script

--- scripts/save_whisper_ewc_params.py
+++ scripts/save_whisper_ewc_params.py
@@ -9,7 +9,10 @@ initialize_env()
 from pathlib import Path
 from safetensors.torch import save_file
 
+import wandb
+
 from trainer.ewc_estimation import get_ewc_params_for_whisper
+from utils.file_io import extract_exp_name_from_model_path
 from utils.constants import CHECKPOINTS_DIR
 
 
@@ -37,6 +40,23 @@ def main(pretrained_model_name_or_path: str = typer.Argument(..., help="The name
          task: str = typer.Option("transcribe", help="The task of the pretrained model."),
          dataset_name: str = typer.Option(..., help="The name of the dataset."),
          batch_size: int = typer.Option(32, help="The batch size for the dataloader.")):
+    
+    # Create config for wandb:
+    config = {
+        "pretrained_model_name_or_path": pretrained_model_name_or_path,
+        "language": language,
+        "task": task,
+        "dataset_name": dataset_name,
+        "batch_size": batch_size
+    }
+    
+    # Initialize W&B:
+    wandb.login()
+    wandb.init(project=os.environ["WANDB_PROJECT_OTHERS"],
+               job_type="save-ewc-params",
+               tags=[dataset_name],
+               name=f"save_ewc_params-{extract_exp_name_from_model_path(pretrained_model_name_or_path)}-{task}-{language}",
+               config=config)
     
     # Get the EWC params:
     mean_params, fisher_params = get_ewc_params_for_whisper(pretrained_model_name_or_path=pretrained_model_name_or_path,
