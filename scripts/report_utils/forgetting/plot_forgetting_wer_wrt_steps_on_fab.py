81827f8ba2ee2a1236779fdd5f15b0d65a6ac37a
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-02 17:43:21 +0100

    Update report util scripts

--- scripts/report_utils/forgetting/plot_forgetting_wer_wrt_steps_on_fab.py
+++ scripts/report_utils/forgetting/plot_forgetting_wer_wrt_steps_on_fab.py
@@ -3,6 +3,7 @@ import typer
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
+from typing import Tuple
 from pathlib import Path
 
 import pandas as pd
@@ -16,36 +17,36 @@ from utils.constants import DEFAULT_OUTPUT_DIR
 sns.set_theme(context="paper", style="ticks")
 
 
-def main(filepath: str, is_relative: bool=False):
+def main(filepath: Path = typer.Argument(..., exists=True, dir_okay=False, file_okay=True, resolve_path=True),
+         is_relative: bool = False,
+         figsize: Tuple[float, float] = typer.Option((6, 4), help="Figure size (width, height).")):
     """
     Script that takes a CSV output from `compare_multiple_models_to_csv.py` and saves
     a plot of the evolution of WER with respect to fine-tuning checkpoints on the
     FAB dataset.
-    
-    Used to generate the table in the "Impact of over-training on forgetting" section
-    of the April Report.
     """
     
-    df = pd.read_csv(filepath).set_index("Dataset")
-    df.index.name = "FAB dataset"
-    df = df.T
-    df.index = df.index.str.extract(r'checkpoint-(\d+)-fab').astype(int).values.flatten()  # type: ignore
+    df = pd.read_csv(filepath).set_index("steps")
     
     if is_relative:
         df = 100 * (df - df.iloc[0]) / df.iloc[0]
     
-    plt.figure(figsize=(6, 4))
-    # plt.figure(figsize=(8, 4))  # poster
-    sns.lineplot(df.iloc[:, :-1], marker="o", dashes=False)
+    plt.figure(figsize=figsize)
+    
+    for col in df.columns:
+        sns.lineplot(data=df, x=df.index, y=col, label=col, marker="o", dashes=False)
+    
     if is_relative:
         plt.axhline(0, color="black", linestyle="--")
+    
+    plt.legend(title="Dataset", loc="best")
     plt.xlabel("Steps")
     plt.ylabel("WER (%)") if not is_relative else plt.ylabel("Relative WER difference (%)")
     
     # Save figure:
-    filename_stem = "compare_wer_multiple_models"
+    filename_stem = "wer_wrt_steps_on_fab"
     if is_relative:
-        filename_stem += "_relative"
+        filename_stem += "-relative"
     savepath = (DEFAULT_OUTPUT_DIR / filename_stem).with_suffix(".png")
     savepath.parent.mkdir(parents=True, exist_ok=True)
     plt.tight_layout()
--- scripts/report_utils/forgetting/plot_tac_forgetting.py
+++ scripts/report_utils/forgetting/plot_tac_forgetting.py
@@ -4,7 +4,6 @@ import os, sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
 from typing import Tuple
-from pathlib import Path
 
 import pandas as pd
 
