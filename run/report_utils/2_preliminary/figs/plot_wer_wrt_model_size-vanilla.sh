9abc6e4b85f9b96ea276fd16ffde4cc3aad5e1bd
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-02 11:16:29 +0100

    Update `plot_wer_wrt_model_size` script

--- run/report_utils/2_preliminary/figs/plot_wer_wrt_model_size-vanilla.sh
+++ run/report_utils/2_preliminary/figs/plot_wer_wrt_model_size-vanilla.sh
@@ -1,5 +1,4 @@
 
 python scripts/report_utils/wer_wrt_model_size/plot_wer_wrt_model_size.py \
-    outputs/report/wer_wrt_model_size/vanilla/wer_wrt_model_size-librispeech.csv \
-    --plot-expected \
-    --savename vanilla/wer_wrt_model_size-librispeech
+    outputs/report/2_preliminary/wer_wrt_model_size/vanilla/wer_wrt_model_size-ami.csv \
+    --plot-expected
