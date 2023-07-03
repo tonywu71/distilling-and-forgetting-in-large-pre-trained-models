95f36c91850e5b56c3e084a9906726a3aebbbbaf
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-03 23:06:45 +0100

    Update report utils for perplexity forgetting

--- run/report_utils/6_implicit_lm_evaluation/figs/plot_implicit_lm_perplexity_wrt_steps-fab.sh
+++ run/report_utils/6_implicit_lm_evaluation/figs/plot_implicit_lm_perplexity_wrt_steps-fab.sh
@@ -1,17 +1,16 @@
 # Generate CSV from eval outputs:
-python scripts/report_utils/compare_wer/compare_wer-multiple_models_to_csv.py \
-	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-500-implicit_lm-perplexity-fab.csv \
-	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-1000-implicit_lm-perplexity-fab.csv \
-	outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-1500-implicit_lm-perplexity-fab.csv \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-2000-implicit_lm-perplexity-fab.csv \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-2500-implicit_lm-perplexity-fab.csv \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3000-implicit_lm-perplexity-fab.csv \
-    outputs/finetuning/whisper_tiny-librispeech_clean_100h-benchmark-freeze_encoder/perplexity/checkpoint-3500-implicit_lm-perplexity-fab.csv
-
+python scripts/report_utils/implicit_lm/concat_csv_forgetting_perplexity_on_fab.py \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-500-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-1000-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-1500-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-2000-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-2500-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-3000-implicit_lm-perplexity-fab.csv \
+    outputs/finetuning/whisper_tiny/compare_freezing_strategies_on_librispeech_clean/librispeech_clean_100h-benchmark-freeze_encoder/implicit_lm_perplexity/implicit_lm_perplexity_wrt_steps_on_fab/checkpoint-3500-implicit_lm-perplexity-fab.csv
 
 # Generate plot from previous CSV:
-python scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
-    outputs/report/compare_perplexity_multiple_models/perplexity_wrt_checkpoints-fab.csv
+python scripts/report_utils/implicit_lm/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
+    outputs/report/6_implicit_lm_evaluation/perplexity_wrt_checkpoints-fab/concat_csv_forgetting_perplexity_on_fab.csv
 
-python scripts/report_utils/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
-    outputs/report/compare_perplexity_multiple_models/perplexity_wrt_checkpoints-fab.csv --is-relative
+python scripts/report_utils/implicit_lm/plot_forgetting_perplexity_wrt_checkpoints_on_fab.py \
+    outputs/report/6_implicit_lm_evaluation/perplexity_wrt_checkpoints-fab/concat_csv_forgetting_perplexity_on_fab.csv --is-relative
