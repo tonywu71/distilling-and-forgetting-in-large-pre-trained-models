fc7749cb888ed433d0a34e5cba44bfc52d538375
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-05 14:09:45 +0100

    Update `initialize_env`

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,27 @@
 from pathlib import Path
 
-# ------ Config ------
+# ------ Env config ------
 DEFAULT_ENV_CONFIG_FILEPATH = "configs/env_config.yaml"
+LIST_ENV_VARS = [
+    "HF_HOME",
+    "TRANSFORMERS_CACHE",
+    "HF_DATASETS_CACHE",
+    "HF_MODULES_CACHE",
+    "CACHE_DIR_LIBRISPEECH",
+    "CACHE_DIR_AMI",
+    "CACHE_DIR_ESB",
+    "CACHE_DIR_ESB_DIAGNOSTIC",
+    "CACHE_DIR_MLS",
+    "WANDB_PROJECT_TRAINING",
+    "WANDB_PROJECT_EVALUATION",
+    "WANDB_PROJECT_OTHERS",
+    "WANDB_CACHE_DIR",
+    "PREPROCESSED_DATASETS_DIR",
+    "K_BEAM_SEARCH_CACHE_DIR"
+]
+
+
+# ------ Config ------
 CHECKPOINTS_DIR = Path("checkpoints/")
 DEFAULT_NUM_PROC = 8  # see https://docs.hpc.cam.ac.uk/hpc/user-guide/a100.html#hardware
 
