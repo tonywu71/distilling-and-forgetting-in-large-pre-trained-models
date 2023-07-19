da1e0d05474754fab79d284802d385a9697a46f1
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-07-19 16:15:03 +0100

    Update configs and scripts

--- run/distil/1_best/distil_1_best-librispeech_100h.sh
+++ run/distil/1_best/distil_1_best-librispeech_100h.sh
@@ -9,8 +9,8 @@
 #!#############################################################
 #! sbatch directives begin here ###############################
 #! Name of the job:
-#SBATCH -J eval_whisper_on_librispeech_clean_test-finetuned
-#! Which project scshould be charged (NB Wilkes2 projects end in '-GPU'):
+#SBATCH -J distil_1_best-librispeech_100h
+#! Which project should be charged (NB Wilkes2 projects end in '-GPU'):
 #SBATCH -A MLMI-tw581-SL2-GPU
 #! How many whole nodes should be allocated?
 #SBATCH --nodes=1
@@ -21,7 +21,7 @@
 #! Note that the job submission script will enforce no more than 32 cpus per GPU.
 #SBATCH --gres=gpu:1
 #! How much wallclock time will be required?
-#SBATCH --time=00:30:00
+#SBATCH --time=06:00:00
 #! What types of email messages do you wish to receive?
 #SBATCH --mail-type=NONE
 #! Uncomment this to prevent the job from being requeued (e.g. if
@@ -41,7 +41,7 @@ LOG=$DIRPATH_EXP/$SLURM_JOB_ID.log
 ERR=$DIRPATH_EXP/$SLURM_JOB_ID.err
 
 
-echo -e "JobID: $JOBID\n======" > $LOG
+echo -e "JobID: $SLURM_JOB_ID\n======" > $LOG
 echo "Time: `date`" >> $LOG
 echo "Running on master node: `hostname`" >> $LOG
 echo "python `which python`": >> $LOG
@@ -51,10 +51,9 @@ echo "python `which python`": >> $LOG
 #! ####                    MAIN                    ###########
 #! ###########################################################
 
-python scripts/eval_whisper.py \
-    checkpoints/distil_1_best/whisper_medium_to_tiny/librispeech_clean_100h/checkpoint-2450 \
-    --dataset-name librispeech_clean \
-    --batch-size 1024 >> $LOG 2> $ERR
+python scripts/distil_whisper.py \
+    configs/distil_configs/1_best/librispeech_100h/distil_1_best-medium_to_tiny-librispeech.yaml \
+    >> $LOG 2> $ERR
 
 #! #############################################
 
