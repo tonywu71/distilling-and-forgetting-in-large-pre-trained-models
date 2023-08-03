Fix inverted attention mask

trainer/trainer_utils.py
@@ -44,21 +44,21 @@ def get_padded_mask_from_tensor(tensor: torch.Tensor) -> torch.Tensor:
     """
     Returns the padded mask from a tensor of shape (batch_size, n_tokens).
     Used convention:
-    - 1 for tokens that are padded
-    - 0 otherwise.
+    - 0 for tokens that are padded
+    - 1 otherwise.
     
     Example:
     - Input: tensor([[50257.,  50362.,     76.,    1694.,    627.,   50256.],
                         [50257.,  50362.,  13099.,   50256.,  50256.,   50256.]])
-    - Output: tensor([[0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 1, 1]])
+    - Output: tensor([[1, 1, 1, 1, 1, 1],
+                      [1, 1, 1, 1, 0, 0]])
     """
     PAD_TOKEN_FROM_GENERATE = 50257  # different from the one used in the tokenizer
     assert tensor.ndim == 2, \
         f"The tensor must be 2D. Got {tensor.ndim} dimensions."
     
     indices = (tensor == PAD_TOKEN_FROM_GENERATE).long().argmax(dim=-1)
-    padded_mask = torch.zeros_like(tensor, dtype=torch.long)
+    padded_mask = torch.ones_like(tensor, dtype=torch.long)
     for idx, row in zip(indices, padded_mask):
-        row[idx+1:] = 1  # ignore the first EOT token of each row
+        row[idx+1:] = 0  # ignore the first EOT token of each row
     return padded_mask