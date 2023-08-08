139354f7ba9528b85042a8a6d2f532885db9427f
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-08-08 15:57:39 +0100

    Remove deprecated eval script for perplexity

--- scripts/eval_whisper.py
+++ scripts/eval_whisper.py
@@ -7,6 +7,7 @@ from utils.initialize import initialize_env
 initialize_env()
 
 from typing import List, Optional
+from pathlib import Path
 from pprint import pprint 
 
 from transformers import set_seed
@@ -14,9 +15,10 @@ import wandb
 
 from dataloader.dataset_for_evaluation.base_dataset_group import BaseDatasetGroup
 from evaluation.eval_dataset_name_to_dataset_group import EVAL_DATASET_NAME_TO_DATASET_GROUP
+from evaluation.eval_whisper_implicit_lm_on_dataset import eval_whisper_implicit_lm_on_dataset
 from evaluation.eval_whisper_on_dataset_group import eval_whisper_on_dataset_group
 from evaluation.eval_whisper_utils import log_wer_to_wandb, save_edit_metrics_to_csv, log_edit_metrics_to_wandb
-from utils.file_io import extract_exp_name_from_model_path
+from utils.file_io import extract_exp_name_from_model_path, extract_output_savepath_from_model_path
 from utils.constants import DEFAULT_EVAL_BATCH_SIZE, DEFAULT_EVAL_NUM_BEAMS
 
 
@@ -37,6 +39,7 @@ def main(pretrained_model_name_or_path: str = typer.Argument(..., help="Path to 
          gen_top_p: Optional[float] = typer.Option(None, help="Top-p for decoding."),
          seed: Optional[int] = typer.Option(None, help="Set seed to reproduce results."),
          savepath: Optional[str] = typer.Option(None, help="Path of the output CSV file. Leave to `None` to use the name of `pretrained_model_name_or_path` as the filename."),
+         eval_implicit_lm_ppl: bool = typer.Option(False, help="Whether to evaluate the implicit language model perplexity or not."),
          save_preds: bool = typer.Option(False, help="Whether to save the predictions in a JSON file or not."),
          debug: bool = typer.Option(False, help="Whether to run in debug mode or not.")) -> None:
     """
@@ -82,14 +85,20 @@ def main(pretrained_model_name_or_path: str = typer.Argument(..., help="Path to 
         "num_beams": num_beams,
         "batch_size": batch_size,
         "savepath": savepath,
+        "eval_implicit_lm_ppl": eval_implicit_lm_ppl,
         "save_preds": save_preds
     }
-    
+
     # Initialize W&B:
     wandb.login()
+
+    tags = [dataset_name]
+    if eval_implicit_lm_ppl:
+        tags.append("implicit_lm_ppl")
+
     wandb.init(project=os.environ["WANDB_PROJECT_EVALUATION"],
                job_type="evaluation",
-               tags=[dataset_name],
+               tags=tags,
                name=f"eval_{dataset_name}-{extract_exp_name_from_model_path(pretrained_model_name_or_path)}",
                config=config,
                mode="disabled" if debug else None)
@@ -113,48 +122,74 @@ def main(pretrained_model_name_or_path: str = typer.Argument(..., help="Path to 
     
     # Evaluate:
     print("Evaluating...")
-    df_edit_metrics = eval_whisper_on_dataset_group(pretrained_model_name_or_path=pretrained_model_name_or_path,
-                                                    ds_group=dataset_group,
-                                                    task=task,
-                                                    zero_shot=zero_shot,
-                                                    batch_size=batch_size,
-                                                    num_beams=num_beams,
-                                                    save_preds=save_preds,
-                                                    generate_kwargs=generate_kwargs)
-    
-    # Round the results:
-    df_edit_metrics = df_edit_metrics.round(2)
-    
-    # Split the results into two dataframes:
-    df_edit_metrics_ortho = df_edit_metrics[["WER ortho (%)", "Sub ortho (%)", "Del ortho (%)", "Ins ortho (%)"]]
-    df_edit_metrics_norm = df_edit_metrics[["WER (%)", "Sub (%)", "Del (%)", "Ins (%)"]]
-    
-    
-    print("\n-----------------------\n")
-    
-    print("Orthometric results:")
-    print(df_edit_metrics_ortho)
-    
-    print("\n-----------------------\n")
-    
-    print("Normalized results:")
-    print(df_edit_metrics_norm)
-    
-    print("\n-----------------------\n")
-    
-    
-    # Save and log the edit metrics:
-    for df, suffix in zip([df_edit_metrics_ortho, df_edit_metrics_norm], ["orthographic", "normalized"]):
-        save_edit_metrics_to_csv(df_edit_metrics=df,
-                                 pretrained_model_name_or_path=pretrained_model_name_or_path,
-                                 dataset_name=dataset_name,
-                                 savepath=savepath,
-                                 suffix=suffix)
-        log_edit_metrics_to_wandb(df_edit_metrics=df, suffix=suffix)
-        if suffix == "normalized":
-            log_wer_to_wandb(wer_metrics=df["WER (%)"], suffix=suffix)
-        else:
-            log_wer_to_wandb(wer_metrics=df["WER ortho (%)"], suffix=suffix)
+
+    if eval_implicit_lm_ppl:
+        results = eval_whisper_implicit_lm_on_dataset(pretrained_model_name_or_path=pretrained_model_name_or_path,
+                                                      ds_group=dataset_group,
+                                                      task=task)
+
+        print("Results:")
+        print(results)
+
+        # Save results:
+        if savepath is None:
+            savepath = extract_output_savepath_from_model_path(pretrained_model_name_or_path) + "-implicit_lm-ppl" + f"-{dataset_name}.csv"
+        
+        Path(savepath).parent.mkdir(exist_ok=True, parents=True)
+        results.to_csv(f"{savepath}")
+        print(f"Results saved to `{savepath}`.")
+        
+        # Log results to W&B:
+        barplot = wandb.plot.bar(wandb.Table(dataframe=results.to_frame().reset_index()),  # type: ignore
+                                label=results.index.name,
+                                value=str(results.name),
+                                title="Per dataset perplexity (%)")
+        wandb.log({"perplexity_for_dataset_group": barplot})
+        wandb.finish()
+
+    else:
+        df_edit_metrics = eval_whisper_on_dataset_group(pretrained_model_name_or_path=pretrained_model_name_or_path,
+                                                        ds_group=dataset_group,
+                                                        task=task,
+                                                        zero_shot=zero_shot,
+                                                        batch_size=batch_size,
+                                                        num_beams=num_beams,
+                                                        save_preds=save_preds,
+                                                        generate_kwargs=generate_kwargs)
+        
+        # Round the results:
+        df_edit_metrics = df_edit_metrics.round(2)
+        
+        # Split the results into two dataframes:
+        df_edit_metrics_ortho = df_edit_metrics[["WER ortho (%)", "Sub ortho (%)", "Del ortho (%)", "Ins ortho (%)"]]
+        df_edit_metrics_norm = df_edit_metrics[["WER (%)", "Sub (%)", "Del (%)", "Ins (%)"]]
+        
+        
+        print("\n-----------------------\n")
+        
+        print("Orthometric results:")
+        print(df_edit_metrics_ortho)
+        
+        print("\n-----------------------\n")
+        
+        print("Normalized results:")
+        print(df_edit_metrics_norm)
+        
+        print("\n-----------------------\n")
+        
+        
+        # Save and log the edit metrics:
+        for df, suffix in zip([df_edit_metrics_ortho, df_edit_metrics_norm], ["orthographic", "normalized"]):
+            save_edit_metrics_to_csv(df_edit_metrics=df,
+                                    pretrained_model_name_or_path=pretrained_model_name_or_path,
+                                    dataset_name=dataset_name,
+                                    savepath=savepath,
+                                    suffix=suffix)
+            log_edit_metrics_to_wandb(df_edit_metrics=df, suffix=suffix)
+            if suffix == "normalized":
+                log_wer_to_wandb(wer_metrics=df["WER (%)"], suffix=suffix)
+            else:
+                log_wer_to_wandb(wer_metrics=df["WER ortho (%)"], suffix=suffix)
     
     wandb.finish()
     
