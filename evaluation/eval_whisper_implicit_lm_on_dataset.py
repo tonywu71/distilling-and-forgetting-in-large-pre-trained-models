14d40ed5ec7b86b46154ae7d89725057807f071c
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-08-09 15:36:04 +0100

    Add batch inference for perplexity eval

--- evaluation/eval_whisper_implicit_lm_on_dataset.py
+++ evaluation/eval_whisper_implicit_lm_on_dataset.py
@@ -2,10 +2,11 @@ import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from typing import Optional
+from typing import Optional, List
+from functools import partial
 
-import numpy as np
 import torch
+from torch.utils.data import DataLoader
 
 import pandas as pd
 from tqdm.auto import tqdm
@@ -17,11 +18,9 @@ from optimum.bettertransformer import BetterTransformer
 
 from dataloader.dataset_for_evaluation.base_dataset_group import BaseDatasetGroup
 from dataloader.collator import DataCollatorSpeechSeq2SeqWithPadding
+from dataloader.preprocessing_train.preprocessing import prepare_dataset_fct
 from models.whisper_zero_cross_attention import WhisperForConditionalGenerationZeroCrossAttention
-from utils.constants import DEFAULT_LABEL_TOKENIZED_COL, DEFAULT_LABEL_STR_COL, DEFAULT_EVAL_BATCH_SIZE
-
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from utils.constants import DEFAULT_LABEL_TOKENIZED_COL, DEFAULT_EVAL_BATCH_SIZE, DEFAULT_NUM_PROC
 
 
 def eval_whisper_implicit_lm_on_dataset_group(pretrained_model_name_or_path: str,
@@ -50,7 +49,7 @@ def eval_whisper_implicit_lm_on_dataset_group(pretrained_model_name_or_path: str
         model = BetterTransformer.transform(model)
 
     # Loop over the datasets:
-    perplexity_results = []
+    ppl_results = []
     tbar = tqdm(ds_group.items())
     
     for dataset_name, dataset in tbar:
@@ -68,34 +67,29 @@ def eval_whisper_implicit_lm_on_dataset_group(pretrained_model_name_or_path: str
         
         feature_extractor = WhisperFeatureExtractor.from_pretrained(pretrained_model_name_or_path)
         
+        prepare_dataset = partial(prepare_dataset_fct,
+                                  tokenizer=tokenizer,
+                                  feature_extractor=feature_extractor)
+        dataset = dataset.map(prepare_dataset, num_proc=DEFAULT_NUM_PROC)
+
         # Load data collator:
         data_collator = DataCollatorSpeechSeq2SeqWithPadding(tokenizer=tokenizer,
                                                              feature_extractor=feature_extractor,
                                                              return_attention_mask=True)
-        
-        # TODO: Currently, perplexity is computed with a batch size of 1. For faster inference, we should
-        #       add support for batch sizes > 1. The only thing to take care of is the padding tokens.
 
-        # Placeholders for per-example perplexities:
-        perplexities_curr_dataset = []
+        dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=data_collator)
+
+        # Placeholders for per-batch perplexities:
+        ppl_per_batch: List[torch.Tensor] = []
         
-        for data in dataset:
-            data = {
-                "input_features": feature_extractor(data["audio"]["array"],
-                                                    sampling_rate=feature_extractor.sampling_rate).input_features[0],  # drop batch dimension
-                DEFAULT_LABEL_TOKENIZED_COL: tokenizer(data[DEFAULT_LABEL_STR_COL]).input_ids
-            }
-            
-            # Collate the data into batches of size 1:
-            data = data_collator([data])
-            
+        for batch in dataloader:
             # Note that we need to move the data to the device manually (which is not the case with Trainer):
-            input_features = data["input_features"].to(device).to(torch_dtype)
-            tokenized_seq = concat_special_tokens(data[DEFAULT_LABEL_TOKENIZED_COL].to(device),
+            input_features = batch["input_features"].to(device).to(torch_dtype)
+            tokenized_seq = concat_special_tokens(batch[DEFAULT_LABEL_TOKENIZED_COL].to(device),
                                                   pretrained_model_name_or_path,
                                                   language=language,
                                                   task=task)
-            attention_mask = data["attention_mask"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
             attention_mask_prefix = torch.Tensor([1, 1, 1, 1]).expand(attention_mask.shape[0], -1).to(attention_mask.device)
             attention_mask = torch.cat([attention_mask_prefix, attention_mask[:, 2:]], dim=1)
 
@@ -107,8 +101,8 @@ def eval_whisper_implicit_lm_on_dataset_group(pretrained_model_name_or_path: str
             # One-step generation:
             with torch.no_grad():
                 output = model.forward(input_features=input_features,
-                                    decoder_input_ids=decoder_input_ids_right_shifted,
-                                    attention_mask=attention_mask_right_shifted)
+                                       decoder_input_ids=decoder_input_ids_right_shifted,
+                                       attention_mask=attention_mask_right_shifted)
             
             # Convert logits to log-probabilities:
             log_prob_all = torch.nn.functional.log_softmax(output.logits, dim=-1)  # (batch_size, seq_len, vocab_size)
@@ -121,17 +115,17 @@ def eval_whisper_implicit_lm_on_dataset_group(pretrained_model_name_or_path: str
             mean_log_prob_seq = log_prob_seq / attention_mask_right_shifted.sum(dim=-1)  # (batch_size,)
             
             # Compute perplexity:
-            perplexity = torch.exp(-mean_log_prob_seq).item()
+            perplexity = torch.exp(-mean_log_prob_seq)  # (batch_size,)
             
             # Add to the list of perplexities:
-            perplexities_curr_dataset.append(perplexity)
+            ppl_per_batch.append(perplexity)
         
         # Add to the list of perplexities:
-        perplexity_results.append(np.mean(perplexities_curr_dataset))
-    
+        ppl_current_dataset = torch.cat(ppl_per_batch, dim=0).mean().item()
+        ppl_results.append(ppl_current_dataset)
     
     # Save the results:
-    results = pd.Series(perplexity_results, index=list(ds_group.keys()), name="Perplexity")
+    results = pd.Series(ppl_results, index=list(ds_group.keys()), name="Perplexity")
     results.index.name = "Dataset"
     
     # Compute the average WER:
