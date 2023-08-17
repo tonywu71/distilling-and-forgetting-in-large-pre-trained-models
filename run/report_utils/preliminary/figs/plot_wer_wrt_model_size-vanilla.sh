9a02bff3ede67037bed201ec09025842ad1c762b
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2023-08-17 12:48:53 +0100

    Update scripts

--- run/report_utils/preliminary/figs/plot_wer_wrt_model_size-vanilla.sh
+++ run/report_utils/preliminary/figs/plot_wer_wrt_model_size-vanilla.sh
@@ -1,4 +1,3 @@
-
 python scripts/report_utils/wer_wrt_model_size/plot_wer_wrt_model_size.py \
-    outputs/report/2_preliminary/wer_wrt_model_size/vanilla/wer_wrt_model_size-ami.csv \
+    outputs/report/preliminary/wer_wrt_model_size/vanilla/wer_wrt_model_size-ami.csv \
     --plot-expected
